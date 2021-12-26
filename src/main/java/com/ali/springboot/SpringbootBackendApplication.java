ad2b53fde2361a314fcd7778099234ee7654cb1c
Ali Habibian <ali.habibian04@gmail.com>
2021-12-26 19:03:46 +0330

    Initial commit

---
+++ src/main/java/com/ali/springboot/SpringbootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ali.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBackendApplication.class, args);
+    }
+
+}
