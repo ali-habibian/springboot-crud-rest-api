71b44c71bd07a02a6c151a388e2e2cfd2f9a409c
Ali Habibian <ali.habibian04@gmail.com>
2021-12-26 19:54:20 +0330

    Create Employee JPA Entity

---
+++ src/main/java/com/ali/springboot/entity/Employee.java
@@ -0,0 +1,23 @@
+package com.ali.springboot.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String email;
+}
